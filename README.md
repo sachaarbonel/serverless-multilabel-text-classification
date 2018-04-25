3025bdd64bb2a79ee3422d444da8e7df6269f76d
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-04-25 15:30:15 +0200

    working

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A ready to deploy example repo as explained in this [blog](https://serverless.co
 
 - Have node and npm installed. There is a good guide for installing node and npm on linux [here](https://github.com/creationix/nvm)
 - Have python and pip installed. There is a good guide for installing python with conda [here](https://conda.io/docs/user-guide/install/linux.html)
+- Have virtualenv installed:
+```
+ pip install virtualenv
+```
 - Make sure you have exported your AWS keys in your environmnet variables
 
 ## Install / Update serverless framework
--- handler.py
+++ handler.py
@@ -1,24 +1,28 @@
 import json
+import tensorflow as tf
+import logging
 
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def add(event, context):
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    logger.info('Event : {}'.format(event))
+    logger.info('Event a : {}'.format(event['a']))
 
-    return response
+    a = tf.constant(event['a'])
+    b = tf.constant(event['b'])
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+    result = tf.Session().run(tf.add(a, b))
+
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "message": "TensorFlow \"add\" function test: {} + {}".format(event['a'], event['b']),
+        "result": "{}".format(result)
     }
-    """
+
+
+    if __name__ == "__main__":
+        main('', '')
